e488fff9d66e5f2e19b547437fe927f4268765a8
abhishek-jar <151015086+abhishek-jar@users.noreply.github.com>
2024-04-12 15:14:05 +0530

    fixed background

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-import showcases from './ asset/showcaseData.js';
-import testimonials from './ asset/testimonialData.js';
+import showcases from "./ asset/showcaseData.js";
+import testimonials from "./ asset/testimonialData.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-
-  const showcaseContainer = document.getElementById('carousel');
-  showcases.forEach(show => {
-    const div = document.createElement('div');
+  const showcaseContainer = document.getElementById("carousel");
+  showcases.forEach((show) => {
+    const div = document.createElement("div");
     div.innerHTML = `<img src="${show.thumbnail}" alt="${show.title}"><p>${show.title}</p>`;
-    div.onclick = () => window.open(show.link, '_blank');
+    div.onclick = () => window.open(show.link, "_blank");
     showcaseContainer.appendChild(div);
   });
 
@@ -16,34 +15,35 @@ document.addEventListener("DOMContentLoaded", function () {
   let currentShowcase = 0;
 
   function autoScroll() {
-    showcaseContainer.scrollBy({ left: showcaseContainer.firstChild.offsetWidth, behavior: 'smooth' });
+    showcaseContainer.scrollBy({
+      left: showcaseContainer.firstChild.offsetWidth,
+      behavior: "smooth",
+    });
     currentShowcase = (currentShowcase + 1) % showcases.length;
     if (currentShowcase === 0) {
-      setTimeout(() => showcaseContainer.scrollTo({ left: 0, behavior: 'smooth' }), 1000);
+      setTimeout(
+        () => showcaseContainer.scrollTo({ left: 0, behavior: "smooth" }),
+        1000
+      );
     }
   }
 
-  autoScrollInterval = setInterval(autoScroll, 3000); // Change interval as needed
-
-  // Stop auto-scroll on user interaction
-  showcaseContainer.addEventListener('touchstart', () => {
-    clearInterval(autoScrollInterval);
-  });
+  autoScrollInterval = setInterval(autoScroll, 3000);
 
   let testimonialIndex = 0;
-  const testimonialContainer = document.getElementById('testimonial');
+  const testimonialContainer = document.getElementById("testimonial");
 
   function displayTestimonial() {
     if (testimonialContainer) {
-      const testimonialDiv = document.createElement('div');
-      testimonialDiv.classList.add('testimonial-div');
+      const testimonialDiv = document.createElement("div");
+      testimonialDiv.classList.add("testimonial-div");
       testimonialDiv.innerHTML = `
           <p>"${testimonials[testimonialIndex].testimonial}"</p><br>
           <p>- ${testimonials[testimonialIndex].name}, ${testimonials[testimonialIndex].country}</p>
       `;
 
       // Clear previous content
-      testimonialContainer.innerHTML = '';
+      testimonialContainer.innerHTML = "";
       testimonialContainer.appendChild(testimonialDiv);
 
       // Immediately make the div visible
@@ -62,14 +62,17 @@ document.addEventListener("DOMContentLoaded", function () {
     console.error("Testimonials array is empty.");
   }
 
-  const whatsappFab = document.getElementById('whatsapp-fab');
-  window.onscroll = function() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  const whatsappFab = document.getElementById("whatsapp-fab");
+  window.onscroll = function () {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
       whatsappFab.style.opacity = "1";
       whatsappFab.style.visibility = "visible";
-  } else {
+    } else {
       whatsappFab.style.opacity = "0";
       whatsappFab.style.visibility = "hidden";
-  }
+    }
   };
 });
