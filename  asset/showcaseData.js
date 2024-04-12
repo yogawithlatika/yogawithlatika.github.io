df7c6724652045c3bac205765670e2c16a20776d
Abhishek Bharadwaj <ab.bharadwaj03@gmail.com>
2024-04-13 01:29:36 +0530

    Improv 130400 (#13)
    * Added front page content
    
    * Update testimonialData.js
    
    * added showcase and index
    
    * updated styles

---  asset/showcaseData.js
+++  asset/showcaseData.js
@@ -1,51 +1,58 @@
 const showcases = [
   {
-    title: "Chair Suryanamaskara",
-    link: "https://youtu.be/zvyDolcCxIY",
-    local: "images/showcase/chair_suryanamaskar.jpeg",
-    thumbnail: "https://img.youtube.com/vi/zvyDolcCxIY/0.jpg",
+    "title": "Yoga for Diabetes",
+    "link": "https://youtu.be/ol-b3cAIuFU",
+    "thumbnail": "https://img.youtube.com/vi/ol-b3cAIuFU/0.jpg"
   },
   {
-    title: "Yoga for PCOS",
-    link: "https://youtu.be/Z9mdDEo7Leo",
-    local: "images/showcase/pcos.jpeg",
-    thumbnail: "https://img.youtube.com/vi/Z9mdDEo7Leo/0.jpg",
+    "title": "Yoga for Hypertension",
+    "link": "https://youtu.be/yML3V2ydTfM",
+    "thumbnail": "https://img.youtube.com/vi/yML3V2ydTfM/0.jpg"
   },
   {
-    title: "Yoga for knee pain",
-    link: "https://youtu.be/SBEz7TldiHs",
-    local: "images/showcase/knee_pain.jpeg",
-    thumbnail: "https://img.youtube.com/vi/SBEz7TldiHs/0.jpg",
+    "title": "Yoga for Cervical Spondylosis",
+    "link": "https://youtu.be/OdiowXHBP6k",
+    "thumbnail": "https://img.youtube.com/vi/OdiowXHBP6k/0.jpg"
   },
   {
-    title: "Yoga for better sleep",
-    link: "https://youtu.be/-BkVO1fV5yw",
-    local: "images/showcase/better_sleep.jpeg",
-    thumbnail: "https://img.youtube.com/vi/-BkVO1fV5yw/0.jpg",
+    "title": "Yoga for Lower Back Pain",
+    "link": "https://youtube.com/shorts/FuAgWciL9Pg",
+    "thumbnail": "https://img.youtube.com/vi/FuAgWciL9Pg/0.jpg"
   },
   {
-    title: "Yoga for back pain",
-    link: "https://youtu.be/CnJnX0L_yBk",
-    local: "images/showcase/backpain.jpeg",
-    thumbnail: "https://img.youtube.com/vi/CnJnX0L_yBk/0.jpg",
+    "title": "Yoga for Migraine",
+    "link": "https://youtu.be/FBTEf1AeLEw",
+    "thumbnail": "https://img.youtube.com/vi/FBTEf1AeLEw/0.jpg"
   },
   {
-    title: "Yoga for migraine",
-    link: "https://youtu.be/FBTEf1AeLEw",
-    local: "images/showcase/migrain.jpeg",
-    thumbnail: "https://img.youtube.com/vi/FBTEf1AeLEw/0.jpg",
+    "title": "Yoga for Thyroid",
+    "link": "https://youtu.be/M42pCotlTJ4",
+    "thumbnail": "https://img.youtube.com/vi/M42pCotlTJ4/0.jpg"
   },
   {
-    title: "Yoga for thyroid",
-    link: "https://youtu.be/M42pCotlTJ4",
-    local: "images/showcase/thyroid.jpeg",
-    thumbnail: "https://img.youtube.com/vi/M42pCotlTJ4/0.jpg",
+    "title": "Yoga for PCOS",
+    "link": "https://youtu.be/Z9mdDEo7Leo",
+    "thumbnail": "https://img.youtube.com/vi/Z9mdDEo7Leo/0.jpg"
   },
   {
-    title: "Yoga for hypertension",
-    link: "https://youtu.be/TxN0Sr7hSqw",
-    local: "images/showcase/hypertension.jpeg",
-    thumbnail: "https://img.youtube.com/vi/TxN0Sr7hSqw/0.jpg",
+    "title": "Yoga for Knee Pain",
+    "link": "https://youtu.be/SBEz7TldiHs",
+    "thumbnail": "https://img.youtube.com/vi/SBEz7TldiHs/0.jpg"
   },
+  {
+    "title": "Morning Yoga for Beginners",
+    "link": "https://youtu.be/yML3V2ydTfM",
+    "thumbnail": "https://img.youtube.com/vi/yML3V2ydTfM/0.jpg"
+  },
+  {
+    "title": "Chair Suryanamaskara",
+    "link": "https://youtu.be/zvyDolcCxIY",
+    "thumbnail": "https://img.youtube.com/vi/zvyDolcCxIY/0.jpg"
+  },
+  {
+    "title": "My Students",
+    "link": "https://youtu.be/DirQHTZ58xI",
+    "thumbnail": "https://img.youtube.com/vi/DirQHTZ58xI/0.jpg"
+  }
 ];
 export default showcases;
