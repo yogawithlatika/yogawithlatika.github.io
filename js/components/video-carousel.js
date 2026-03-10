89b49372ed19f6f516b9d07c5fa7653354da7b87
yogawithlatika <140300720+yogawithlatika@users.noreply.github.com>
2026-03-10 07:06:21 +0530

    Added new videos

--- js/components/video-carousel.js
+++ js/components/video-carousel.js
@@ -1,36 +1,24 @@
 // js/components/video-carousel.js
 const videoData = [
   {
-    id: "1motM-CZZds",
-    title: "Bedtime Yoga for Deep Sleep"
-  },
-  {
     id: "ZcwZFpBXma0",
     title: "Yoga to reduce stress, improve sleep"
-  },
-  {
-    id: "rWDD0TWZXSI",
-    title: "Yoga for Long Sitting Hours"
   },
   {
     id: "LAYgeIKm8m0",
     title: "Yoga for Back, Neck & Shoulder Pain Relief"
   },
   {
-    id: "ZjNRm6jCN7U",
-    title: "Chair Suryanamaskara | Desk Yoga"
-  },
-  {
-    id: "WxcFTNNo3Ro",
-    title: "Yoga Stretches for Tech Neck & Upper Back Tension"
-  },
-  {
-    id: "kPH9Lo2oMj4",
-    title: "Yoga for Neck & Cervical Pain Relief"
+    id: "nwK-a0FJbEU",
+    title: "Prenatal Guided Meditation"
   },
   {
     id: "ZuuvIa85QGI",
-    title: "Yoga to Stop Morning Knee Stiffness & Knee Pain"
+    title: "Yoga for Knee Stiffness & Pain"
+  },
+  {
+    id: "dB66ygiGTbA",
+    title: "10 minute Guided Chanting Meditation"
   }
 ];
 
